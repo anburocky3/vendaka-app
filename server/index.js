0ee6e734d848f1aa6580171c4ab985a7a8a20cd8
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2026-04-08 17:20:53 +0530

    feat: connecting frontend with backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+// import cors from "cors";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
+// app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
@@ -9,9 +12,13 @@ app.get("/", (req, res) => {
 
 // create a end point for register user
 app.post("/register", (req, res) => {
+  const { name, email, password } = req.body; // { name, email, password }
+
   // Here you would typically handle user registration logic,
   // such as validating input, hashing passwords, and storing user data in a database.
-  res.json({ message: "User registration endpoint" });
+  res.json({
+    message: `User ${name} registered successfully with email ${email}`,
+  });
 });
 
 app.listen(PORT, () => {
--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/:path*", // Your Node.js backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
