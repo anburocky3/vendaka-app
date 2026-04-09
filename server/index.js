4b2e121a168a5fdcb1ad58a19f844fcadec3e888
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2026-04-09 18:01:26 +0530

    feat(database): setup MongoDB connection and environment
    - Add .env.example for MongoDB URI configuration
    - Update .gitignore to exclude .env file
    - Create db/connection.js for MongoDB connection logic
    - Implement /movies endpoint to fetch movies from database
    - Enhance /register endpoint to hash passwords using bcrypt
    - Add dotenv for environment variable management
    - Create utils/loadEnvironment.js to load environment variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 // import cors from "cors";
-
+import "./utils/loadEnvironment.js";
+import db from "./db/connection.js";
+import bcrypt from "bcrypt";
 const PORT = process.env.PORT || 5000;
 const app = express();
 // app.use(cors());
@@ -10,15 +12,40 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
+// for testing the database connection
+app.get("/movies", async (req, res) => {
+  try {
+    const movies = await db.collection("movies").find().toArray();
+    res.json(movies);
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    res.status(500).json({ error: "Failed to fetch movies" });
+  }
+});
+
 // create a end point for register user
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body; // { name, email, password }
 
-  // Here you would typically handle user registration logic,
-  // such as validating input, hashing passwords, and storing user data in a database.
-  res.json({
-    message: `User ${name} registered successfully with email ${email}`,
-  });
+  // do some basic validation
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Name, email, and password are required" });
+  }
+
+  try {
+    await db
+      .collection("users")
+      .insertOne({ name, email, password: bcrypt.hashSync(password, 10) });
+
+    res.json({
+      message: `User ${name} registered successfully with email ${email}`,
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Failed to register user" });
+  }
 });
 
 app.listen(PORT, () => {
