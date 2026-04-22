610860fe5534f4ce3fe5be03cd490878b0243671
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2026-04-22 19:00:30 +0530

    feat(products): integrate product fetching and update UI
    - Replace static product data with dynamic fetching from the database.
    - Enhance ProductList component with improved layout and styling.
    - Remove unnecessary welcome message from Dashboard.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,28 +1,9 @@
-const products = [
-  {
-    id: 1,
-    name: "Drawing Tablet",
-    image:
-      "https://astropad.com/wp-content/uploads/2024/01/Wacom-Cintiq-16-.webp",
-    price: 8499,
-  },
-  {
-    id: 2,
-    name: "Gaming Keyboard",
-    image:
-      "https://static0.howtogeekimages.com/wordpress/wp-content/uploads/2022/04/rgb-lit-gaming-mechanical-keyboard.jpg?w=1200&h=675&fit=crop",
-    price: 1999,
-  },
-  {
-    id: 3,
-    name: "PlayStation 5",
-    image: "https://m.media-amazon.com/images/I/51ljnEaW0pL.jpg",
-    price: 50000,
-  },
-];
+import Product from "#models/admin/product.model";
 
-export function getProducts(req, res) {
+export async function getProducts(req, res) {
   const loggedInUser = req.user;
+
+  const products = await Product.find();
 
   res.json({
     products,
--- ui/src/app/(auth)/dashboard/page.jsx
+++ ui/src/app/(auth)/dashboard/page.jsx
@@ -3,9 +3,6 @@ import ProductList from "./ProductList";
 export default function Dashboard() {
   return (
     <div className="p-10 mt-18">
-      <h1 className="text-4xl font-bold text-gray-800">
-        Welcome to the Dashboard
-      </h1>
       <div>
         <ProductList />
       </div>
