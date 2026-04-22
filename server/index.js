28dee1e2767835b6686b827b6b94ec55edb7c0ec
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2026-04-22 19:05:52 +0530

    feat(rate-limit): add rate limiting middleware
    - Implement express-rate-limit to control request rates
    - Add test for rate limiting functionality

--- server/index.js
+++ server/index.js
@@ -4,12 +4,25 @@ import "#utils/loadEnvironment";
 import apiRouter from "#routers/index.route";
 import "#db/connection";
 import path from "path";
+import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 // app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(import.meta.dirname, "uploads")));
+
+// rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window`
+  message: "Too many requests, please try again later.",
+  standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
+  legacyHeaders: false, // Disable `X-Rate-Limit-*` headers
+});
+
+// Apply the rate limiter to all requests
+app.use(limiter);
 
 /// Routers
 app.use("/", apiRouter);
